a301857da53b070e6296243962aafd295828acd7
Gonmel <gonzaspcba@gmail.com>
2020-05-15 22:16:53 -0300

    Templates modificados para mostrar post desde base de datos.

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,4 @@ def post_detail(request, pk):
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'posts': posts})
+    return render(request, 'blog/post_detail.html', {'post': post})
